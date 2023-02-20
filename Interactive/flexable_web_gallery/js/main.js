0af2394d9477e55e0796fba50eed01c6001c4e39
Kim-sodam <sodamk812@gmail.com>
2023-02-20 15:16:20 +0900

    sventh commit

--- Interactive/flexable_web_gallery/js/main.js
+++
@@ -1,9 +0,0 @@
-let $grid = $('.grid').imagesLoaded( function() {
-  // init Isotope after all images have loaded
-  $grid.isotope({
-    // options...
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-item',
-    percentPosition: true
-  });
-});
